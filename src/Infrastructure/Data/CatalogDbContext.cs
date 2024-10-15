5bc946285f2632f2a6c527663af660f898c91f55
23456-Tony-Larrea <avs-larrea@yavirac.edu.ec>
2024-10-15 08:12:39 -0500

    test shop

---
+++ src/Infrastructure/Data/CatalogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorShared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+        }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/ListLocalidadController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Microsoft.eShopWeb.Infrastructure.Data;
+[ApiController]
+[Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+public class ListLocalidadController : ControllerBase
+{
+    private readonly CatalogDbContext _context;
+
+    public ListLocalidadController(CatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{localidad}")]
+    public IActionResult GetProductosPorLocalidad(string localidad)
+    {
+        var productos = _context.CatalogItems.Where(p => p.Localidad == localidad).ToList();
+        return Ok(productos);
+    }
+}
+
